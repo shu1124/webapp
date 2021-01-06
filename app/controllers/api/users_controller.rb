b2d018d26519f39b20f4cf9f68badfc9ced0ed19
shu1124 <shu.suzuki1124@gmail.com>
2021-01-07 02:08:05 +0900

    user index action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,12 @@
 class Api::UsersController < ApplicationController
+  PER_PAGE = 12
+  def index
+    users = User.order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+    render json: users, each_serializer: UserSerializer, meta: { total_pages: users.total_pages,
+                                                                           total_count: users.total_count,
+                                                                           current_page: users.current_page }
+  end
+  
   def create
     user = User.new(user_params)
     user.save!
