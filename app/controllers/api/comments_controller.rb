cbe1b882dc4c405d98f655b19ef6ee03dc952d32
shu1124 <shu.suzuki1124@gmail.com>
2021-01-07 17:05:12 +0900

    setting-backend

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,24 @@
+class Api::CommentsController < ApplicationController
+
+  def index
+    comments = Comment.where(micropost_id: params[:micropost_id]).order("id DESC")
+    render json: comments
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user.id
+    if comment.save
+      render json: comment
+    else
+      render json: comment.errors, status: 422
+    end
+
+  end
+
+  private
+  def comment_params
+    params.permit(:content).merge(user_id: current_user.id, micropost_id: params[:micropost_id])
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
-
+  
   has_many :microposts, dependent: :destroy
   has_one_base64_attached :avatar
-
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
+  has_many :comments
 
   scope :by_name, ->(name) { where('name LIKE ?', "%#{name}%") }
   scope :by_tag, ->(tag_ids) { joins(:user_tags).where(user_tags: { tag_id: tag_ids }) }
