0c7b0b42ab367a5003af8062eb50d124bb2ba33e
shu1124 <shu.suzuki1124@gmail.com>
2021-01-11 16:59:15 +0900

    install rspec

--- test/controllers/api/microposts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/micropost_genre_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MicropostGenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
